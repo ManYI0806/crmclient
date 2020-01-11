45c07d3d9f2f7b67615fd4a609519ab724bcfc47
XTL <59634973+ManYI0806@users.noreply.github.com>
2020-01-11 13:51:39 +0800

    Update Welcome.vue
    修改components组件里的welcome组件视图

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,21 +1,42 @@
 <template>
-<div>
-	<el-breadcrumb separator-class="el-icon-arrow-right">
-		<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-		<el-breadcrumb-item>活动管理</el-breadcrumb-item>
-		<el-breadcrumb-item>活动列表</el-breadcrumb-item>
-		<el-breadcrumb-item>活动详情</el-breadcrumb-item>
-	</el-breadcrumb>
-</div>
+	<div>
+		<el-breadcrumb separator="/">
+			<el-breadcrumb-item :to="{ path: '/index/' }">
+				<i class="el-icon-s-home"></i>
+			</el-breadcrumb-item>
+			<el-breadcrumb-item>欢迎页面</el-breadcrumb-item>
+		</el-breadcrumb>
+		<img src="../assets/welcome.jpg" />
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld'
-}
+	export default {
+		name: 'Welcome'
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+	.el-breadcrumb {
+		background-color: #D9EDF7;
+		border-radius: 5px;
+		height: 36px;
+		line-height: 36px;
+		padding-left: 15px;
+		font-weight: 00;
+	}
+
+	.el-icon-s-home {
+		font-size: 18px;
+	}
+	
+	div{
+		text-align: center;
+	}
+	img{
+		margin-top: 80px;
+	}
+	
 </style>
