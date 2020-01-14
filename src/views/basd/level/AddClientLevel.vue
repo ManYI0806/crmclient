3a7addb03d4496e6a2c9fcdf4ff284b671f0d740
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 10:26:39 +0800

    Update AddClientLevel.vue
    编写AddClientLevel页面

--- src/views/basd/level/AddClientLevel.vue
+++ src/views/basd/level/AddClientLevel.vue
@@ -1,11 +1,52 @@
 <template>
 	<div>
-		addClientLevel新建客户等级
+		<el-breadcrumb separator="/">
+			<el-breadcrumb-item><i class="el-icon-s-home"></i>
+			</el-breadcrumb-item>
+			<el-breadcrumb-item>基础数据</el-breadcrumb-item>
+			<el-breadcrumb-item>客户等级管理</el-breadcrumb-item>
+			<el-breadcrumb-item>新建客户等级</el-breadcrumb-item>
+		</el-breadcrumb>
+		<el-form ref="dataClientLevel" :model="dataClientLevel" label-width="100px" style="margin-top: 15px;width: 500px;">
+			<el-form-item label="客户等级名称">
+				<el-input v-model="dataClientLevel.dclName"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<el-button type="primary" @click="add()">保存</el-button>
+			</el-form-item>
+		</el-form>
 	</div>
 </template>
 
 <script>
+	export default {
+       data() {
+			return {
+				dataClientLevel: {
+					dclId:'',
+					dclName:''
+				}
+			}
+		},
+		methods: {
+			add() {
+				this.$axios.post('insertDataClientLevel', this.dataClientLevel)
+					.then((response) => {
+						if (response.data == 1) {
+							this.$router.push('/index/dataClientLevel');
+							console.log(response);
+						} else {
+							alert('添加失败！');
+						}
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+			}
+		}
+	}
 </script>
 
 <style>
+
 </style>
