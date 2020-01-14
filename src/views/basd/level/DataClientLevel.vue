53210514305d9ac67984d97a0b50ff4daea593d6
13840272115 <59674995+13840272115@users.noreply.github.com>
2020-01-14 10:26:27 +0800

    Update DataClientLevel.vue
    编写DataClientLevel页面

--- src/views/basd/level/DataClientLevel.vue
+++ src/views/basd/level/DataClientLevel.vue
@@ -7,9 +7,9 @@
 			<el-breadcrumb-item>企业客户等级管理</el-breadcrumb-item>
 		</el-breadcrumb>
 
-		<el-table :data="dataClientLevellist" stripe style="width: 100%">
+		<el-table :data="dataClientLevellist" stripe style="width: 100%" @selection-change="handleSelectionChange">
 			<el-table-column>
-				<el-checkbox v-model="checked"></el-checkbox>
+				<el-table-column type="selection" width="55"></el-table-column>
 			</el-table-column>
 			<el-table-column prop="dclId" label="编号" width="180">
 			</el-table-column>
@@ -17,13 +17,13 @@
 			</el-table-column>
 			<el-table-column>
 				<template slot-scope="scope">
-					<el-button type="primary">编辑</el-button>
-					<el-button type="danger">删除</el-button>
+					<el-button type="primary" @click="editdataClientLevel(scope.row)">编辑</el-button>
+					<el-button type="danger" @click="deldataClientLevel(scope.row)">删除</el-button>
 				</template>
 			</el-table-column>
 		</el-table>
-		<el-button type="primary">新建</el-button>
-		<el-button type="danger">批量删除</el-button>
+		<el-button type="primary" @click="adddataClientLevel()">新建</el-button>
+		<el-button type="danger" @click="delBatch()">批量删除</el-button>
 	</div>
 </template>
 
@@ -32,7 +32,8 @@
 		data() {
 			return {
 				checked: false,
-				dataClientLevellist: []
+				dataClientLevellist: [],
+				multipleSelection:[]
 			}
 		},
 		created() {
@@ -43,12 +44,71 @@
 				this.$axios.post('selectDataClientLevelAll')
 					.then((response) => {
 						this.dataClientLevellist = response.data;
-						console.log(response.data);
+//						console.log(response.data);
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+			},
+			adddataClientLevel() {
+				this.$router.push('/index/addClientLevel');
+			},
+			deldataClientLevel(row) {
+				if(!confirm('确定要删除此数据吗？')) {
+					return;
+				}
+				this.$axios.post('deleteDataClientLevel', {
+						dclId: row.dclId
+					})
+					.then((response) => {
+						if(response.data == 1) {
+							this.init();
+						} else {
+							alert('删除失败！');
+						}
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+			},
+			handleSelectionChange(val) {
+				this.multipleSelection = val;
+			
+			},
+			editdataClientLevel(row) {
+				this.$router.push({
+					path: '/index/editClientLevel',
+					query: {
+						dclId: row.dclId
+					}
+				});
+
+			},
+			delBatch(){
+				if(this.multipleSelection.length<=0){
+					alert('请选择要删除的数据！');
+					return;
+				}
+				if(!confirm('确认要删除这些数据吗？')){
+					return;
+				}
+				let paramArr = [];
+				for(let i=0;i<this.multipleSelection.length;i++){
+					paramArr[i]=this.multipleSelection[i].dclId;
+				}
+				this.$axios.post('deleteDataClientLevelBatch', paramArr)
+					.then((response) => {
+						if(response.data>0){
+							this.init();
+						}else{
+							alert('删除失败！');
+						}
 					})
 					.catch((error) => {
 						console.log(error);
 					});
 			}
+
 		}
 	}
 </script>
