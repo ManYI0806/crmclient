65fda074afd88f7780b08837b0a7f3c9a799a1b2
XTL <59634973+ManYI0806@users.noreply.github.com>
2020-01-16 11:48:21 +0800

    Update App.vue
    在App中添加provide，实现当前页面刷新的效果

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+	<div id="app">
+		<router-view v-if="isReload" />
+	</div>
 </template>
 
+<script>
+	export default {
+		name: "App",
+		provide() {
+			return {
+				reload: this.reload
+			}
+		},
+		data() {
+			return {
+				isReload: true
+			}
+		},
+		methods: {
+			reload() {
+				this.isReload = false
+				this.$nextTick(() => {
+					this.isReload = true
+				})
+			}
+		}
+	}
+</script>
+
 <style>
+
 </style>
